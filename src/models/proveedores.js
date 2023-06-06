c9d40105516b794c0e92847e9e546a67b05676c5
Raul Belloso Medina <rbelloso@aeropuertosgap.com.mx>
2023-06-06 17:56:22 -0600

    proveedores migrations and users uuid

--- src/models/proveedores.js
+++ src/models/proveedores.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Proveedores extends Model {
     /**
@@ -13,17 +11,45 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Proveedores.init({
-    proveedor: DataTypes.STRING,
-    encargado: DataTypes.STRING,
-    email: DataTypes.STRING,
-    address: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    logo: DataTypes.TEXT,
-    status: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Proveedores',
-  });
+  Proveedores.init(
+    {
+      proveedor: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      encargado: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: { msg: "El email debe ser un correo válido" },
+        },
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      logo: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Proveedores",
+    }
+  );
   return Proveedores;
 };
