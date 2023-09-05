98cfe03fe6c82331734229a30be58c7c906f3387
RaulBellosoM <raul.belloso.m@gmail.com>
2023-09-05 14:05:53 -0600

    fix activo

--- src/controllers/inventary.controller.js
+++ src/controllers/inventary.controller.js
@@ -66,7 +66,7 @@ export const getValidateActivoSN = async (req, res) => {
       where: {
         [Op.and]: [
           {
-            [Op.or]: [{ serialNumber: serialNumber }],
+            [Op.or]: [{ activo: activo }, { serialNumber: serialNumber }],
           },
           {
             id: {
