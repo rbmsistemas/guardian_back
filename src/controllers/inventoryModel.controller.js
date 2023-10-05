922a996fe0f0c54686eb07d65b4b7081e579f04d
RaulBellosoM <raul.belloso.m@gmail.com>
2023-10-05 17:16:50 -0600

    enabled models module funcitons

--- src/controllers/inventoryModel.controller.js
+++ src/controllers/inventoryModel.controller.js
@@ -1,14 +1,91 @@
 import db from "../models/index.js";
 
 const InventoryModel = db.InventoryModel;
+const Inventory = db.Inventory;
+const InventoryType = db.InventoryType;
+const InventoryBrand = db.InventoryBrand;
 
 export const getInventoryModels = async (req, res) => {
   try {
     const inventoryModels = await InventoryModel.findAll({
       order: [["name", "ASC"]],
+      include: [
+        {
+          model: InventoryType,
+          as: "inventoryType",
+        },
+        {
+          model: InventoryBrand,
+          as: "inventoryBrand",
+        },
+      ],
     });
     res.json({
       inventoryModels,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error al obtener los modelos de inventario",
+      error,
+    });
+  }
+};
+
+export const getInventoryModelsByParams = async (req, res) => {
+  const {
+    inventoryTypeId,
+    inventoryBrandId,
+    search,
+    page,
+    quantityResults,
+    orderBy,
+    sort,
+  } = req.body;
+
+  let order = [["name", "DESC"]];
+
+  try {
+    let where = {};
+    if (inventoryTypeId) where.inventoryTypeId = inventoryTypeId;
+    if (inventoryBrandId) where.inventoryBrandId = inventoryBrandId;
+    if (search) where.name = search;
+
+    if (orderBy && sort) {
+      if (orderBy === "name") {
+        order = [[orderBy, sort]];
+      } else if (orderBy === "inventoryType") {
+        order = [[InventoryType, "name", sort]];
+      } else if (orderBy === "inventoryBrand") {
+        order = [[InventoryBrand, "name", sort]];
+      } else {
+        order = [[orderBy, sort]];
+      }
+    }
+
+    const inventoryModels = await InventoryModel.findAll({
+      where,
+      order,
+      offset: page * quantityResults,
+      limit: quantityResults,
+      include: [
+        {
+          model: InventoryType,
+          as: "inventoryType",
+        },
+        {
+          model: InventoryBrand,
+          as: "inventoryBrand",
+        },
+      ],
+    });
+
+    const totalResults = await InventoryModel.count({ where });
+    const totalPages = Math.ceil(totalResults / quantityResults);
+    res.json({
+      inventoryModels,
+      totalResults,
+      totalPages,
     });
   } catch (error) {
     console.log(error);
@@ -52,5 +129,32 @@ export const updateInventoryModel = async (req, res) => {
       message: "Error al actualizar el modelo de inventario",
       error,
     });
+  }
+};
+
+export const getInventoryModelById = async (id) => {
+  try {
+    const inventoryModel = await InventoryModel.findByPk(id, {
+      include: [
+        {
+          model: InventoryType,
+          as: "inventoryType",
+        },
+        {
+          model: InventoryBrand,
+          as: "inventoryBrand",
+        },
+      ],
+    });
+
+    const inventories = await Inventory.findAll({
+      where: {
+        inventoryModelId: id,
+      },
+    });
+    return { inventoryModel, inventories };
+  } catch (error) {
+    console.log(error);
+    return error;
   }
 };
--- src/routes/inventoryModel.routes.js
+++ src/routes/inventoryModel.routes.js
@@ -1,9 +1,15 @@
 import Router from "express";
 import { verifyToken } from "../middleware/index.js";
-import { getInventoryModels } from "../controllers/inventoryModel.controller.js";
+import {
+  getInventoryModels,
+  getInventoryModelById,
+  getInventoryModelsByParams,
+} from "../controllers/inventoryModel.controller.js";
 
 const router = Router();
 
 router.get("/inventoryModels", verifyToken, getInventoryModels);
+router.get("/inventoryModels/:id", verifyToken, getInventoryModelById);
+router.post("/inventoryModels/search", verifyToken, getInventoryModelsByParams);
 
 export default router;
